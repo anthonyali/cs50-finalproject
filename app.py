95e2f31d619eff06c1a08104192b75b72145cbff
Anthony Ali <94753443+anthonyali@users.noreply.github.com>
2022-03-14 21:44:25 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from cs50 import SQL
 from flask import Flask, render_template, request
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///farms.db")
 
 @app.route("/")
 def index():
